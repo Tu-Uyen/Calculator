1512905c2181f826385bc8c9bc1cf7121338db48
Tu Uyen <2054052082uyen@ou.edu.vn>
2022-10-16 14:19:52 +0700

    Update form1

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -28,12 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btCong = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtKetqua = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btCong
+            // 
+            this.btCong.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCong.Location = new System.Drawing.Point(356, 177);
+            this.btCong.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btCong.Name = "btCong";
+            this.btCong.Size = new System.Drawing.Size(66, 31);
+            this.btCong.TabIndex = 0;
+            this.btCong.Text = "+";
+            this.btCong.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Số a:";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(130, 29);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(255, 29);
+            this.txtA.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Số b:";
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(130, 75);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(255, 29);
+            this.txtB.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 24);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Kết quả:";
+            // 
+            // txtKetqua
+            // 
+            this.txtKetqua.Location = new System.Drawing.Point(130, 123);
+            this.txtKetqua.Name = "txtKetqua";
+            this.txtKetqua.Size = new System.Drawing.Size(255, 29);
+            this.txtKetqua.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(467, 223);
+            this.Controls.Add(this.txtKetqua);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btCong);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Calculate";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btCong;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtKetqua;
     }
 }
 
